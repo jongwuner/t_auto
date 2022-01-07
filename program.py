3127117996af671254dba97d851656a88c5ad854
이종원 <jklh0202@naver.com>
2022-01-08 02:52:28 +0900

    Update program.py

--- program.py
+++ program.py
@@ -10,7 +10,7 @@ import xmltodict
 import FinanceDataReader as fdr
 from pyowm import OWM
 
-# date1
+# date
 today = datetime.datetime.now(timezone('Asia/Seoul'))
 date1 = today.strftime('%Y-%m-%d')
 date2 = today.strftime('%Y년 %m월 %d일'.encode('unicode-escape').decode()).encode().decode('unicode-escape')
